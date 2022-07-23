ea9f5d55beb8915b3c17d8e59612ee502e5e7934
muhammad-jiku <muhammadjiku364@gmail.com>
2022-07-23 18:45:23 +0600

    multiple routes added and pages, authetication, shared folder added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import SignIn from './Pages/Authentication/SignIn/SignIn';
+import SignUp from './Pages/Authentication/SignUp/SignUp';
+import Home from './Pages/Home/Home/Home';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello there</h1>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="siginin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
